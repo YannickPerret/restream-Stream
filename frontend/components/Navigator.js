76df415d07a89ada913932cdb2f0a4329f15eab1
Yannick Perret <contact@yannickperret.com>
2024-04-30 09:24:44 +0200

    feat : create first nav bar

--- frontend/components/Navigator.js
+++ frontend/components/Navigator.js
@@ -1,23 +1,27 @@
-import { Home, ListVideo, SquareGanttChart, Tv2, Video } from "lucide-react";
+import { Home, ListVideo, LogIn, SquareGanttChart, Tv2, Video } from "lucide-react";
+import Link from "next/link";
 
 export default function Navigator() {
   return (
     <nav className="flex-1 p-5 space-y-2">
-      <a href="#" className="flex items-center p-2 text-white hover:bg-gray-700 rounded-md align-baseline">
+      <Link href="/" className="flex items-center p-2 text-white hover:bg-gray-700 rounded-md align-baseline">
         <Home size={20} /> Home
-      </a>
-      <a href="#" className="flex items-center p-2 text-white hover:bg-gray-700 rounded-md align-baseline">
+      </Link>
+      <Link href="#" className="flex items-center p-2 text-white hover:bg-gray-700 rounded-md align-baseline">
         <Tv2 size={20} /> Streams
-      </a>
-      <a href="#" className="flex items-center p-2 text-white hover:bg-gray-700 rounded-md align-baseline">
+      </Link>
+      <Link href="#" className="flex items-center p-2 text-white hover:bg-gray-700 rounded-md align-baseline">
         <SquareGanttChart size={20} /> Timelines
-      </a>
-      <a href="#" className="flex items-center p-2 text-white hover:bg-gray-700 rounded-md align-baseline">
+      </Link>
+      <Link href="#" className="flex items-center p-2 text-white hover:bg-gray-700 rounded-md align-baseline">
         <ListVideo size={20} /> Playlists
-      </a>
-      <a href="#" className="flex items-center p-2 text-white hover:bg-gray-700 rounded-md align-baseline">
+      </Link>
+      <Link href="#" className="flex items-center p-2 text-white hover:bg-gray-700 rounded-md align-baseline">
         <Video size={20} /> Vidéos
-      </a>
+      </Link>
+      <Link href="/login" className="flex items-center p-2 text-white hover:bg-gray-700 rounded-md align-baseline">
+        <LogIn size={20} /> Login
+      </Link>
     </nav>
   )
 }
