3e5b2b2fc40af5cc223df5f98e9b19ee28f7dd53
Yannick Perret <contact@yannickperret.com>
2024-05-01 11:52:23 +0200

    feat : use Link nextjs lib and conditionnal auth link

--- frontend/components/Navigator.js
+++ frontend/components/Navigator.js
@@ -1,12 +1,45 @@
-import { Home, ListVideo, LogIn, SquareGanttChart, Tv2, Video } from "lucide-react";
+'use client';
+import { Home, ListVideo, LogIn, LogOut, SquareGanttChart, Tv2, Video } from "lucide-react";
 import Link from "next/link";
+import useSessionStore from "../stores/useSessionStore";
+import { useRouter } from 'next/navigation'
 
 export default function Navigator() {
+  const isAuthenticated = useSessionStore(state => state.isAuthenticated());
+  const session = useSessionStore(state => state.session);
+  const router = useRouter();
+
+  async function handleLogout(e) {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/logout`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          "Authorization": session ? `${session.token.type} ${session.token.token}` : "",
+        },
+
+      });
+
+      if (response.ok) {
+        useSessionStore.getState().logout();
+        router.push('/login');
+      } else {
+        throw new Error('Logout failed');
+      }
+    } catch (error) {
+      console.error('Logout error', error);
+    }
+  }
+
   return (
     <nav className="flex-1 p-5 space-y-2">
       <Link href="/" className="flex items-center p-2 text-white hover:bg-gray-700 rounded-md align-baseline">
         <Home size={20} /> Home
       </Link>
+      {isAuthenticated ? (
+        <>
       <Link href="#" className="flex items-center p-2 text-white hover:bg-gray-700 rounded-md align-baseline">
         <Tv2 size={20} /> Streams
       </Link>
@@ -19,9 +52,19 @@ export default function Navigator() {
       <Link href="#" className="flex items-center p-2 text-white hover:bg-gray-700 rounded-md align-baseline">
         <Video size={20} /> Vidéos
       </Link>
+      <button
+        type="button"
+        className="flex items-center p-2 text-white hover:bg-gray-700 rounded-md align-baseline"
+        onClick={handleLogout}
+      >
+        <LogOut size={20} /> Logout
+      </button>
+        </>
+        ) : (
       <Link href="/login" className="flex items-center p-2 text-white hover:bg-gray-700 rounded-md align-baseline">
         <LogIn size={20} /> Login
       </Link>
+        )}
     </nav>
   )
 }
