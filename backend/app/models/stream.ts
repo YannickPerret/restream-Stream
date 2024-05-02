31106b1825c31f4c4286d0779dc2d334f3c22e7c
Yannick Perret <contact@yannickperret.com>
2024-05-02 15:29:26 +0200

    feat: add relation streams

--- backend/app/models/stream.ts
+++ backend/app/models/stream.ts
@@ -1,9 +1,29 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import User from '#models/user'
 
 export default class Stream extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare pid: number | null
+
+  @column()
+  declare status: 'active' | 'inactive'
+
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+
+  @column.dateTime()
+  declare startTime: DateTime | null
+
+  @column.dateTime()
+  declare endTime: DateTime | null
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
