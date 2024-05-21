81c3b7f1eeb66e787223dad953d940ac17f5400f
Yannick Perret <contact@yannickperret.com>
2024-05-21 09:43:09 +0200

    feet : add transmit configuration

--- backend/config/transmit.ts
+++ backend/config/transmit.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from '@adonisjs/transmit'
+import { middleware } from '#start/kernel'
 
 export default defineConfig({
-  pingInterval: false,
-  transport: null
+  pingInterval: '10s',
+  transport: null,
+
+  /*routeHandlerModifier(route) {
+    // Ensure you are authenticated to register your client
+    if (route.getPattern() === '/__transmit/events') {
+      route.middleware(middleware.auth())
+      return
+    }
+  },*/
 })
