4c96530bee99a8b78c355ef4c992a74b1b98bbb7
Yannick Perret <contact@yannickperret.com>
2024-05-21 16:28:47 +0200

    chore : add not found page

---
+++ frontend/src/app/(protected)/not-found.js
@@ -0,0 +1,18 @@
+'use client'
+export default function NotFound () {
+    return (
+        <section className="flex flex-col w-full h-full rounded-2xl justify-center shadow-2xl">
+            <div className="bg-slate-500">
+                <header className="container mx-auto">
+                    <h1 className="text-3xl text-white py-4">Streams</h1>
+                    <hr className="border-b-1 border-blueGray-300 pb-6"/>
+                </header>
+
+                <div>
+                    Authentication
+                    error 404
+                </div>
+            </div>
+        </section>
+    )
+}
