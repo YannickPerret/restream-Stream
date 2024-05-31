efdd379001591af7340a5d72897895674eb508cd
Yannick Perret <contact@yannickperret.com>
2024-05-31 22:06:11 +0200

    feat : add schedule to remove video when guest token expired

--- backend/start/scheduler.ts
+++ backend/start/scheduler.ts
@@ -2,6 +2,9 @@ import scheduler from 'adonisjs-scheduler/services/main'
 import Stream_manager from '#models/stream_manager'
 import app from '@adonisjs/core/services/app'
 import ace from '@adonisjs/core/services/ace'
+import GuestToken from '#models/guest_token'
+import { DateTime } from 'luxon'
+import * as fs from 'node:fs'
 
 scheduler
   .call(async () => {
@@ -21,6 +24,20 @@ scheduler
     }
   })
   .everyFifteenMinutes()
+
+scheduler
+  .call(async () => {
+    const guestTokens = await GuestToken.query().where('expires_at', '<', DateTime.local().toSQL())
+    for (const guestToken of guestTokens) {
+      const videos = await guestToken.related('video').query()
+      for (const video of videos) {
+        fs.unlinkSync(video.path)
+        await video.delete()
+      }
+      await guestToken.delete()
+    }
+  })
+  .everyHours(1)
 
 /*
  23 May 2024 - Perret - Code to start the scheduler when server up by Tom Gobich https://discord.com/channels/423256550564691970/423256550564691972/1242858943185682586
