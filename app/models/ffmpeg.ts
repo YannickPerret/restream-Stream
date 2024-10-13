0a2c6261e1ae69e0c17bf55cf8885515a914835a
tchoune <contact@yannickperret.com>
2024-10-13 17:18:10 +0200

    chore : remove comment

--- app/models/ffmpeg.ts
+++ app/models/ffmpeg.ts
@@ -70,7 +70,6 @@ export default class FFMPEGStream {
       encodingParameters.push('-f', 'tee', teeOutput);
     }
 
-    console.log('FFmpeg command:', 'ffmpeg', [...inputParameters, ...encodingParameters]);
     this.instance = spawn('ffmpeg', [...inputParameters, ...encodingParameters], {
       detached: true, stdio: ['ignore', 'pipe', 'pipe']
     });
